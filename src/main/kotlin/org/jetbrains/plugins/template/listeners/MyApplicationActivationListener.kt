8291e98b2e7808301cea3af22826101abc04b4f8
Matthew Gharrity <gharrma@google.com>
2024-09-09 14:11:24 -0700

    Repro module dependency violation
    Observed behavior:
    
    * `./gradlew assemble` succeeds
    
    * `./gradlew verifyPlugin` reports no issue
    
    * At runtime, a ClassNotFoundException is thrown when accessing class
      com.intellij.smartUpdate.VcsUpdateStep.
    
    Expected behavior:
    
    * Compilation should fail; module 'intellij.smart.update' should not
      be on the compile classpath.
    
    * The intellij-plugin-verifier should complain that classes from
      'intellij.smart.update' are being used without a corresponding
      module dependency declaration in plugin.xml

--- src/main/kotlin/org/jetbrains/plugins/template/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/listeners/MyApplicationActivationListener.kt
@@ -8,5 +8,14 @@ internal class MyApplicationActivationListener : ApplicationActivationListener {
 
     override fun applicationActivated(ideFrame: IdeFrame) {
         thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+        // Check if classes from module 'intellij.smart.update' are accessible.
+        try {
+            thisLogger().warn("Starting experiment")
+            com.intellij.smartUpdate.VcsUpdateStep() // This class is from ${idea.home}/lib/modules/intellij.smart.update.jar.
+            thisLogger().warn("VcsUpdateStep was accessed without issue")
+        } catch (e: Throwable) {
+            thisLogger().warn("Failed to access VcsUpdateStep")
+            thisLogger().error(e)
+        }
     }
 }
